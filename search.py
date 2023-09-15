4a5649f6b8f10fb180b822278a1f11d6fb416316
Sarah Kam <sarahskam@gmail.com>
2023-09-15 13:33:03 -0400

    binary search

---
+++ search.py
@@ -0,0 +1,15 @@
+def binary_search(arr, item):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = low + high // 2   # integer division rounds down
+        if arr[mid] == item:
+            return mid
+        elif arr[mid] < item:   # guess was smaller than item
+            low = mid + 1       # set the new lower bound to mid+1
+        elif arr[mid] > item:   # guess was larger than item
+            high = mid - 1      # set the new upper bound to mid-1
+    return None
+
+myList = [1, 5, 7, 9]
+print(binary_search(myList, 3))
