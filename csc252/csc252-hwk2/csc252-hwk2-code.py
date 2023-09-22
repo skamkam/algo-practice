6ebb636c5cc5dfe6216ae413cc96efeba0b496e2
Sarah Kam <sarahskam@gmail.com>
2023-09-22 13:32:16 -0400

    working on hwk2

---
+++ csc252/csc252-hwk2/csc252-hwk2-code.py
@@ -0,0 +1,76 @@
+# Name:  Sarah Kam
+# Peers:  N/A
+# References:  N/A
+import time
+import random
+
+# INCLUDE TEST CASES TO SUCCEED *AND* FAIL
+
+### DO NOT EDIT ###
+def new_array(size: int):
+    L = [0] * size
+    return L
+### END ###
+
+def random_array(size: int):    # for testing; creates given size arr of rand nums from 0-99
+    L = new_array(size)
+    for i in range(size):
+        L[i] = random.randint(0, 100)
+    return L
+
+# Problem 1(a): SWAP (COPY)
+
+def swapcopy(arr, pos1, pos2):      # this is O(n) because have to copy the whole array
+    copyarr = new_array(len(arr))   # create new array the size of old arr
+    for i in range(len(arr)):
+        if i == pos1:
+            copyarr[i] = arr[pos2]
+        elif i == pos2:
+            copyarr[i] = arr[pos1]
+        else:
+            copyarr[i] = arr[i]
+    return copyarr
+
+# Problem 1(b): SWAP (IN PLACE)
+
+def swapinplace(arr, pos1, pos2):   # this is O(1)
+    temp = arr[pos1]
+    arr[pos1] = arr[pos2]     # perform the swaps
+    arr[pos2] = temp
+    return arr
+
+# Problem 2: SUM OF FIRST n INTEGERS
+
+def gaussiansum(n):     # O(1)
+    return n * (n-1) / 2
+
+def sumfirstnnums(n):   # O(n)
+    ans = 0
+    for i in range(n+1):
+        ans += i
+    return ans
+
+#Problem 3: SUM OF ARRAY OF NUMBERS
+
+def main():
+    # Creates the first n numbers in the Fibonacci sequence.
+    n = 10      # Try for numbers up to 100000. 
+    start_time_create = time.time()    # Get start Time.
+    M = new_array(n)
+    M[0] = 1
+    M[1] = 1
+    for j in range(2, len(M)):
+        M[j] = M[j-1] + M[j-2]
+    end_time_create = time.time()      # Get end Time.   
+    print("%s seconds to create %d number of the Fibonacci sequence" % ((end_time_create - start_time_create),n))    
+    
+
+    randlist = random_array(100)
+    print(swapcopy(randlist, 2, 4))
+    print(swapinplace(randlist, 2,4))
+    
+   
+    
+
+if __name__ == "__main__":    
+    main()
